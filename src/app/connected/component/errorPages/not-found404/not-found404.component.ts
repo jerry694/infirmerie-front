27f49c8c548dbc3ba64f87b736782db8f8089ebf
jerry694 <wafoaristide@gmail.com>
2023-09-09 18:54:33 +0200

    fix:redirection

--- src/app/connected/component/errorPages/not-found404/not-found404.component.ts
+++ src/app/connected/component/errorPages/not-found404/not-found404.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found404',
   templateUrl: './not-found404.component.html',
   styleUrls: ['./not-found404.component.scss']
 })
-export class NotFound404Component {
-
+export class NotFound404Component implements OnInit{
+  constructor(private router:Router){}
+    ngOnInit(): void {
+      setTimeout(() => {
+      this.router.navigate(["dashboard"]);
+    }, 2000);
+    }
 }
