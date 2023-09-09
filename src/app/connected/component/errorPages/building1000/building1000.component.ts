88ac191839b1549da8bc51b63e0dd8c911609e6e
jerry694 <wafoaristide@gmail.com>
2023-09-09 10:47:23 +0200

    fix:building page

--- src/app/connected/component/errorPages/building1000/building1000.component.ts
+++ src/app/connected/component/errorPages/building1000/building1000.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-building1000',
   templateUrl: './building1000.component.html',
   styleUrls: ['./building1000.component.scss']
 })
-export class Building1000Component {
+export class Building1000Component implements OnInit{
+constructor(private router:Router){}
+  ngOnInit(): void {
+    setTimeout(() => {
+    this.router.navigate(["dashboard"]);
+  }, 2000);
+  }
 
 }
